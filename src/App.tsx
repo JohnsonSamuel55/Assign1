ab20213b0353567756c56e05c107d51d121d4146
Samuel Johnson <97634659+JohnsonSamuel55@users.noreply.github.com>
2023-01-23 17:30:46 -0700

    API Calls Work

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css'
 
-
-function noRefresh(event){
-  event.preventDefault();
+interface Quote{
+  _id: string;
+  author: string;
+  content: string;
 }
 
-export function App() {
+export function App () {
+  const [result, setResult] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [resultQuotes, setResultQuotes] = useState<Quote[]>([]);
+  const [randomQuote, setRandomQuote] = useState<Quote | null>(null);
 
-  return (
-    <div className="App">
-      <form onSubmit={noRefresh}>
-        <label for="Name">Name</label>
-        <input type="text" id="Name" name="Name" /><br />
-	    <input type="submit" value="Submit" />
-      </form> 
+  function noRefresh(event){
+    setResult(searchTerm);
+    event.preventDefault();
+  }
+
+  async function loadRandomQuote() {
+    const fetchResult = await fetch("https://usu-quotes-mimic.vercel.app/api/random");
+    const quote = await fetchResult.json();
+    console.log(quote)
+    setRandomQuote(quote);
+  }
+
+  async function loadQuotes() {
+    console.log(searchTerm);
+    if (searchTerm === "") return;
+    const fetchResult = await fetch(`https://usu-quotes-mimic.vercel.app/api/search?query=${searchTerm}`);
+    const fullResults = await fetchResult.json();
+    const quotes = await fullResults.results;
+    console.log(quotes);
+    if (quotes.count === 0) return;
+    setResultQuotes(quotes);
+  }
+  {/* Uncomment this to use the API
+    useEffect(() => {
+      loadQuotes()
+    }, [result]);
+
+    useEffect(() => {
+    loadRandomQuote()
+  },[]);
+*/}
+
+return (
+    <div>
+      <div>
+        <form onSubmit={noRefresh}>
+          <label htmlFor="Search">Search: </label>
+          <input  
+            type="text" 
+            name="Search" 
+            value={searchTerm} 
+            onChange={e => setSearchTerm(e.target.value)} />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      <div>
+        <p>{randomQuote?.author}</p>
+        <p>{randomQuote?.content}</p>
+      </div>
+      {
+        resultQuotes.map((resultQuote)=> (
+          <div key={resultQuote._id}>
+            <p>{resultQuote?.author}</p>
+            <p>{resultQuote?.content}</p>
+          </div>
+        ))
+      }
     </div>
-  )
+  );
 }
+
