ed49a9499281a7d23dbfe5e868ed6f565fb429ef
Samuel Johnson <97634659+JohnsonSamuel55@users.noreply.github.com>
2023-01-26 17:44:28 -0700

    Should be done

--- src/App.tsx
+++ src/App.tsx
@@ -21,21 +21,18 @@ export function App () {
   async function loadRandomQuote() {
     const fetchResult = await fetch("https://usu-quotes-mimic.vercel.app/api/random");
     const quote = await fetchResult.json();
-    console.log(quote)
     setRandomQuote(quote);
   }
 
   async function loadQuotes() {
-    console.log(searchTerm);
     if (searchTerm === "") return;
     const fetchResult = await fetch(`https://usu-quotes-mimic.vercel.app/api/search?query=${searchTerm}`);
     const fullResults = await fetchResult.json();
     const quotes = await fullResults.results;
-    console.log(quotes);
     if (quotes.count === 0) return;
     setResultQuotes(quotes);
   }
-  {/* Uncomment this to use the API
+  
     useEffect(() => {
       loadQuotes()
     }, [result]);
@@ -43,30 +40,31 @@ export function App () {
     useEffect(() => {
     loadRandomQuote()
   },[]);
-*/}
+
 
 return (
     <div>
-      <div>
+      <div className='Search'>
+        <h1>Quotes Search</h1> 
         <form onSubmit={noRefresh}>
-          <label htmlFor="Search">Search: </label>
           <input  
-            type="text" 
-            name="Search" 
+            type="text"
+            className='SearchBar'
             value={searchTerm} 
             onChange={e => setSearchTerm(e.target.value)} />
-          <input type="submit" value="Submit" />
+            <br/>
+          <input className='Submit' type="submit" />
         </form>
       </div>
-      <div>
-        <p>{randomQuote?.author}</p>
+      <div className='RandomQoute'>
         <p>{randomQuote?.content}</p>
+        <p>{randomQuote?.author}</p>
       </div>
       {
         resultQuotes.map((resultQuote)=> (
-          <div class="Results" key={resultQuote._id}>
-            <p>{resultQuote?.author}</p>
+          <div className='Result' key={resultQuote._id}>
             <p>{resultQuote?.content}</p>
+            <p>{resultQuote?.author}</p>
           </div>
         ))
       }
